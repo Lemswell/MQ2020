9ff7ba4e59d6677e6e31857c8755633ab8856886
Lemuel <42715753+Lemswell@users.noreply.github.com>
2021-10-10 17:39:35 +1100

    Create COMP3010A2.java
    Got a start on my assignment.

---
+++ COMP3010A2.java
@@ -0,0 +1,37 @@
+import java.util.*
+
+public class COMP3010A2 {
+
+    // This assignment consists of finding and optimizing the breaking down of strings into substrings with number of breaks 'b' on string 's'
+
+    public ArrayList<Integer> BasicBreakIdxOrdering(String s, ArrayList<Integer> b) {
+        ArrayList<Integer> sb = new ArrayList<Integer>(); // breakpoints sorted in optimal order
+        
+        // Basic method: the distance (in terms of index) between highest 'break' and 's' length is compared to the distance between lowest 'break' and 0
+        //               the 'break' with the highest distance is added, and 
+        //               the process is repeated with new given substring.
+        
+        ArrayList<Integer> bCopy = new ArrayList<Integer>(); // copy of break indicies
+        
+        for(Integer i : b) {
+            bCopy.add(i);
+        }
+        
+        int low = 0;
+        int high = s.length()-1;
+        
+        // note that 'breaks' are inputted in ascending order
+        while(bCopy.size()>0){
+            if(bCopy.get(0) - low > high - bCopy.get(b.size()-1)) {
+                sb.add(bCopy.get(0));
+                low = bCopy.get(0);
+                bCopy.remove(0);
+            } else {
+                sb.add(bCopy.get(b.size()-1));
+                high = bCopy.get(b.size()-1);
+                bCopy.remove(b.size()-1);
+            }
+        }
+        return sb;
+    }
+}
